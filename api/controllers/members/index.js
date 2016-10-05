49598a392ab32453e2362d7226ef835871b11b4f
Simon Sun <blackghost0118@gmail.com>
2016-10-05 15:15:52 +0800

    增加驗證端點

--- api/controllers/members/index.js
+++ api/controllers/members/index.js
@@ -3,11 +3,15 @@ let router = express.Router();
 
 import signup from './signup';
 import signin from './signin';
+import verify from './verify';
 
 router.route('/members/signup')
     .post(signup);
 
 router.route('/members/signin')
     .post(signin);
+
+router.route('/members/verify')
+    .get(verify);
 
 module.exports = router;
