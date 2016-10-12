fdd8d787a6e001771b3518ebd447ce9583c20ee3
wayne1025 <wayne1025.tw@hotmail.com>
2016-10-12 18:38:24 +0800

    會員認證信件端點改變

--- admin/controllers/members/signup.js
+++ admin/controllers/members/signup.js
@@ -69,7 +69,7 @@ module.exports = (req, res, next) => {
         // 讀取會員認證信的樣板
         let html = nunjucks.render('./mailTemplates/confirm.html', {
             expireTime: moment(expireTime).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm:ss'),
-            verifiedLink: `http://m.nownews.com?verify=${verifyToken.token}`
+            verifiedLink: `http://localhost:7777/member?verify=${verifyToken.token}`
         });
 
         // 非同步送出會員認證信
