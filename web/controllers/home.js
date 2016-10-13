5c97bf4205cdf9f05d364da6e2478b8e806fdcb6
WebberWu <bird23074035@gmail.com>
2016-10-13 16:40:22 +0800

    沒有使用者就倒到登入或註冊頁面

--- web/controllers/home.js
+++ web/controllers/home.js
@@ -3,6 +3,9 @@ let router = express.Router();
 
 router.route('/')
     .get((req, res, next) => {
+        if (!req.session.user) {
+            return res.redirect('members/signup');
+        }
         return res.render('home/home');
     });
 
