e9bf7ef9baa09d7b9aa2b0edff95d257189699a4
WebberWu <bird23074035@gmail.com>
2016-10-13 16:45:20 +0800

    路徑寫錯了 QQ

--- web/controllers/home.js
+++ web/controllers/home.js
@@ -4,7 +4,7 @@ let router = express.Router();
 router.route('/')
     .get((req, res, next) => {
         if (!req.session.user) {
-            return res.redirect('members/signup');
+            return res.redirect('members/sign');
         }
         return res.render('home/home');
     });
