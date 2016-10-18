4f6650a2459fb855e452ef9afdaa4e975ce65f60
Simon Sun <blackghost0118@gmail.com>
2016-10-18 13:08:48 +0800

    調整一些 code

--- global.js
+++
@@ -1,4 +0,0 @@
-global.rootPath = __dirname;
-global.config = {
-    apiServer: 'http://localhost:8888'
-};
--- web/util/postApi.js
+++ web/util/postApi.js
@@ -1,7 +1,9 @@
 import fetch from 'node-fetch';
 
+const config = require('../../config')[`${process.env.NODE_ENV}`];
+
 module.exports = function(url, body) {
-    let fetchUrl = `${config.apiServer}/${url}`;
+    let fetchUrl = `${config.api.url}/${url}`;
 
     return fetch(fetchUrl, {
             timeout: 10000,
