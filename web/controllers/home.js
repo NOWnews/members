175ad556d699fdd13aa440ef3817e15b7b81bdb9
wayne1025 <wayne1025.tw@hotmail.com>
2016-10-19 16:22:26 +0800

    更改網址及增加home頁面

--- web/controllers/home.js
+++ web/controllers/home.js
@@ -4,9 +4,9 @@ let router = express.Router();
 router.route('/')
     .get((req, res, next) => {
         let user = req.session.user;
-        if (!user) {
-            return res.redirect('members/sign');
-        }
+        // if (!user) {
+        //     return res.redirect('members/sign');
+        // }
         return res.render('home/home', user);
     });
 
